58228293b4e01faab7a3885966e7b86f985bb692
SahanPerera96 <dsahanperera@gmail.com>
2019-05-18 05:28:25 +0530

    Get,Post,Put,Delete Methods Createdfor OpenStock

--- stock-management/src/main/java/com/stock/management/controller/OpenStockController.java
+++ stock-management/src/main/java/com/stock/management/controller/OpenStockController.java
@@ -1,5 +1,65 @@
 package com.stock.management.controller;
 //----- Sahan Part -----
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stock.management.modal.OpenStock;
+import com.stock.management.modal.OpenStockDetails;
+import com.stock.management.service.OpenStockService;
+
+
+@RestController
+@RequestMapping(value = "/openstock")
 public class OpenStockController {
 
+	@Autowired
+    OpenStockService stockService;
+
+	// create  stock log with its respective details
+	@RequestMapping(value = "/openStockAll", method = RequestMethod.POST)
+    public OpenStock saveOpenStock(@RequestBody OpenStock openStock) {
+
+        return stockService.saveOpenStock(openStock);
+    }
+	// fetch all stock logs with its respective stock details
+    @RequestMapping(value = "/openStockAll", method = RequestMethod.GET)
+    public List<OpenStock> fetchAllOpenStock() {
+
+        return stockService.fetchAllOpenStock();
+    }
+    
+    // create a new stock log only
+    @RequestMapping(value = "/openStockLog", method = RequestMethod.POST)
+    public OpenStock createOpenStock(@RequestBody OpenStock openStock) {
+
+        return stockService.createOpenStock(openStock);
+    }
+    
+    // create a new open stock detail entry for an existing stock log // not validated 
+    @RequestMapping(value = "/openStockDetails/{id}", method = RequestMethod.POST)
+    public OpenStockDetails createOpenStockDetails(@PathVariable Integer id, @RequestBody OpenStockDetails details) {
+
+    	return stockService.createOpenStockDetails(id, details);
+    }
+    
+    // update existing stock details entry
+    @RequestMapping(value = "/openStockDetails/{id}", method = RequestMethod.PUT)
+    public void updateOpenStockDetails(@PathVariable Integer id, @RequestBody OpenStockDetails details){
+     
+    	stockService.updateOpenStockDetails(id, details);
+    }
+    
+    // delete existing stock details entry
+    @RequestMapping(value = "/openStockDetails/{id}", method = RequestMethod.DELETE)
+    public void deleteOpenStockDetails(@PathVariable Integer id){
+     
+    	stockService.deleteOpenStockDetails(id);
+    }
 }
--- stock-management/src/main/java/com/stock/management/modal/OpenStock.java
+++ stock-management/src/main/java/com/stock/management/modal/OpenStock.java
@@ -1,5 +1,66 @@
 package com.stock.management.modal;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 //----- Sahan Part -----
+@Entity
 public class OpenStock {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id = 0;
+
+    int date;
+    String user;
+    String reason;
+
+    @OneToMany(mappedBy = "openStock", cascade = CascadeType.ALL)
+    List<OpenStockDetails> openStockDetails;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<OpenStockDetails> getOpenStockDetails() {
+        return openStockDetails;
+    }
+
+    public void setOpenStockDetails(List<OpenStockDetails> openStockDetails) {
+        this.openStockDetails = openStockDetails;
+    }
 }
