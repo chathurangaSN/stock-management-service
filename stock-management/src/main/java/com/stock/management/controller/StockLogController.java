53e6ca1f1a6492648d8e84f6ed1d1a915d4204b1
ChathurangaSN <sachithmaths@gmail.com>
2019-05-21 08:23:02 +0530

    i tried with thymeleaf

--- stock-management/src/main/java/com/stock/management/controller/StockLogController.java
+++ stock-management/src/main/java/com/stock/management/controller/StockLogController.java
@@ -2,7 +2,10 @@ package com.stock.management.controller;
 
 import java.util.List;
 
+import javax.swing.text.html.HTML;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,9 +28,15 @@ public class StockLogController {
 	public StockLog save(@RequestBody StockLog stockLog){
 		return stockLogService.save(stockLog);
 	}
+//	@RequestMapping(value ="/getstocklog", method = RequestMethod.GET)
+//	public List<StockLog> fetchAllStocks(){
+//		return stockLogService.fetchAllStocks();
+//	}
+	
 	@RequestMapping(value ="/getstocklog", method = RequestMethod.GET)
-	public List<StockLog> fetchAllStocks(){
-		return stockLogService.fetchAllStocks();
+	public String fetchAllStocks(Model model){
+		 model.addAllAttributes(stockLogService.fetchAllStocks());
+		return "stockLog";
 	}
 	
 	
